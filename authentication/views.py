f558c5382da26ae8c94a5c8c99afa05e4569bd7f
Boyuan Liu <boyuanliu6@yahoo.com>
2021-01-20 18:53:05 -0800

    added basic account django project

---
+++ authentication/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+# Create your views here.
+def register(request):
+    return render(request, "authentication/register.html")
+
+
+def login_view(request):
+    pass
+
+
+def logout_view(request):
+    pass
+
